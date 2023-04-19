0e78c71e1c6369affddb15e9c7172a331d78b5dc
ziwen <ziwentan@umich.edu>
2023-04-19 06:43:21 -0400

    Added Radius filtering and docstrings

--- application.py
+++ application.py
@@ -1,17 +1,11 @@
 from hotelAPI import create_app
 from flask_cors import CORS
 from flask_socketio import SocketIO
-from contextlib import redirect_stderr
-from urllib import response
-from flask import Blueprint,request,render_template,redirect,url_for,Response,session
-
-
+from flask import  render_template
 import os
-
 import json
-import warnings
 import sqlite3
-from flask_socketio import Namespace, emit
+from flask_socketio import  emit
 
 application = create_app()
 socketio = SocketIO(application)
@@ -19,24 +13,55 @@ CORS(application)
 
 url = "db.sqlite"
 
-
-
-
-
 def dbConnection(url):
+    """
+    Establishes a connection to the SQLite database.
+
+    Args:
+        url (str): URL or path of the SQLite database.
+
+    Returns:
+        connection (sqlite3.Connection): SQLite database connection object.
+    """
     connection = sqlite3.connect(url)
     return connection
 
 @application.route('/')
 def index():
-    return render_template ('index.html')
+    """
+    Renders the index.html template for the root URL.
+
+    Returns:
+        Response: Flask response object containing the rendered HTML template.
+    """
+    return render_template('index.html')
 
 @application.route('/hotelfinder', methods=['POST', 'GET'])
 def tester():
+    """
+    Renders the hotelmap.html template for the /hotelfinder URL.
+
+    Returns:
+        Response: Flask response object containing the rendered HTML template.
+    """
     return render_template('hotelmap.html')
 
 @socketio.on('category')
 def handle_my_custom_event(json_data):
+    """
+    Event handler for handling 'category' event from the client.
+
+    This function is triggered when a 'category' event is emitted from the client.
+    It retrieves the selected amenity from the JSON data, checks if a cached JSON file
+    exists for that amenity, and either returns the cached data or queries the database
+    for the data and caches it for future use.
+
+    Args:
+        json_data (dict): JSON data containing the selected amenity value.
+
+    Returns:
+        None
+    """
     amenity = json_data['selectedValue']
     cache_file = "{}.json".format(amenity) # Cache file name based on amenity
     if os.path.exists(cache_file):
@@ -60,4 +85,4 @@ def handle_my_custom_event(json_data):
         return emit('response', result_list)
 
 if __name__ == '__main__':
-    socketio.run(application,debug=True)
+    socketio.run(application, debug=True)
